f722d94139303ea642f8a174b13588d975e3b3fe
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2023-09-29 17:54:33 +0200

    All tests passed, ex05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let finalSum = 0;
+    if (x > y){
+        [x, y] = [y, x];
+    } else if (x < 0 || y < 0 || typeof x == !'number' || typeof y !== 'number'){
+        return "ERROR";
+    }
+    for (let i = x; i <= y; i++){
+        finalSum += i;
+    }
+    return finalSum;
+}
 
-};
+sumAll(1, 4)
 
 // Do not edit below this line
 module.exports = sumAll;
