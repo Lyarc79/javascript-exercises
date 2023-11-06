f1dd42cb0dae9c3ba19ec27c43b5886a2a42c11e
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2023-11-06 15:21:50 +0100

    All tests passed, 9th ex completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (word) {
+   word = word.toLowerCase();
+   let cleanWord = '';
+   for (let i = 0; i < word.length; i++){
+    const char = word[i];
+    if((char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')){
+        cleanWord += char;
+    }
+   }
+   let start = 0;
+   let end = cleanWord.length -1;
 
+   while (start < end){
+    if (cleanWord[start] !== cleanWord[end]){
+        return false;
+    }
+    start ++;
+    end--; 
+   }
+   return true;
 };
 
 // Do not edit below this line
