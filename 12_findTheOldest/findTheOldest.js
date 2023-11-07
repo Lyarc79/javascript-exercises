77cb84ead5321e265a7a51fd17485a3b0c4c17de
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2023-11-07 18:50:50 +0100

    All tests passed, ex 12th done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    
+    const oldestPerson = people.reduce((oldest, person) => {
+        const age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfBirth;
 
+        if(!oldest || age > oldest.age) {
+            return {age, person};
+        } else {
+            return oldest;
+        }
+    }, null);
+    return oldestPerson.person;
 };
 
 // Do not edit below this line
