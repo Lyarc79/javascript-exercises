06c1cd8bc41ab6bb4129c2d2012b16dc823075a7
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2023-11-07 13:17:27 +0100

    All test passed, ex 10th completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    n = Number(n);
+    if (n === 1 || n === 2) {
+        return 1;
+    } else if (n < 0){
+        return "OOPS";
+    } else {
+        const nMinus1 = fibonacci(n - 1);
+        const nMinus2 = fibonacci(n - 2);
+        const result = nMinus1 + nMinus2;
+        return result;
+    }
 };
 
 // Do not edit below this line
